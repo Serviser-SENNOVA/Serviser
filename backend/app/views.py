e9d3d2d42df3ba6664b5104d558269e9a3cdd077
Valwolfor <diegofelipe.gallo@gmail.com>
2024-04-05 20:31:12 -0700

    view

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import AppSerializer
+from .models import User
 
 # Create your views here.
+class AppView(viewsets.ModelViewSet):
+    serializer_class = AppSerializer
+    queryset = User.objects.all()
